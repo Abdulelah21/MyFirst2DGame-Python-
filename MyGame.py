d7cd408d2e9f2a6055d5f07418276382015494e1
Abdulelah21 <78698991+Abdulelah21@users.noreply.github.com>
2022-04-23 16:11:54 +0300

    Adding Rectangles and player folder

--- MyGame.py
+++ MyGame.py
@@ -13,7 +13,12 @@ ground_surface = pygame.image.load('graphiks/ground.png').convert()
 text_surface = test_font.render('My First game',False,'Black')# it take (text informtion,anti-aliase,color)
 
 snail_surface = pygame.image.load('graphiks/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomright = (600,300))
+
+
+player_surf = pygame.image.load('graphiks/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,9 +28,11 @@ while True:
     screen.blit(sky_surface,(0,0)) #it taik (name,postion)
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
-    snail_x_pos -= 5
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos,270))
+
+    snail_rect.x -= 5
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surface,snail_rect)
+    screen.blit(player_surf,player_rect)
 
     #update everything 
     pygame.display.update() 
