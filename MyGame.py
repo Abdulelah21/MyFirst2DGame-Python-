ae26136e081b85ce472f59793df4802b2fc4e76d
Abdulelah21 <78698991+Abdulelah21@users.noreply.github.com>
2022-04-23 22:56:57 +0300

    creating different game states

--- MyGame.py
+++ MyGame.py
@@ -6,6 +6,7 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('My2D_Game')#To change the title name 
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf',50)# it take (type , size)
+game_active = True
 
 #elements surfaces
 sky_surface = pygame.image.load('graphiks/Sky.png').convert()
@@ -28,33 +29,48 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-            #adding mouse events
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos): 
-                player_gravity = -20
 
-        #player input
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-               player_gravity = -20
+        if game_active:        
+                #adding mouse events
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.bottom >= 300:
+                    if player_rect.collidepoint(event.pos): 
+                        player_gravity = -20
+
+            #player input
+            if event.type == pygame.KEYDOWN:
+                if player_rect.bottom >= 300:
+                    if event.key == pygame.K_SPACE:
+                        player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800                             
 
         
+    if game_active:
+        #draw all our elements
+        screen.blit(sky_surface,(0,0)) #it taik (name,postion)
+        screen.blit(ground_surface,(0,300))
+        pygame.draw.rect(screen,'#c0e8ec',score_rect)
+        pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+        screen.blit(score_surf,score_rect)
 
-    #draw all our elements
-    screen.blit(sky_surface,(0,0)) #it taik (name,postion)
-    screen.blit(ground_surface,(0,300))
-    pygame.draw.rect(screen,'#c0e8ec',score_rect)
-    pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
-    screen.blit(score_surf,score_rect)
+        snail_rect.x -= 5
+        if snail_rect.right <= 0: snail_rect.left = 800
+        screen.blit(snail_surface,snail_rect)
 
-    snail_rect.x -= 5
-    if snail_rect.right <= 0: snail_rect.left = 800
-    screen.blit(snail_surface,snail_rect)
+        #Player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300: player_rect.bottom = 300
+        screen.blit(player_surf,player_rect)
 
-    #Player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    screen.blit(player_surf,player_rect)
+        #collision  
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Yellow')        
 
 
 
