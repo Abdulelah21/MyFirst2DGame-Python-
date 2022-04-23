709c362ef69f5bc8368c97b36ae6464b13a7a949
Abdulelah21 <78698991+Abdulelah21@users.noreply.github.com>
2022-04-23 23:18:22 +0300

    Creating Score

--- MyGame.py
+++ MyGame.py
@@ -1,19 +1,26 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = test_font.render(f'Score: {current_time}',False,(64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf,score_rect)
+
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('My2D_Game')#To change the title name 
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf',50)# it take (type , size)
 game_active = True
+start_time = 0
 
 #elements surfaces
 sky_surface = pygame.image.load('graphiks/Sky.png').convert()
 ground_surface = pygame.image.load('graphiks/ground.png').convert()
 
-score_surf = test_font.render('My First game',False,(64,64,64))# it take (text informtion,anti-aliase,color)
-score_rect = score_surf.get_rect(center = (400,50))
+#score_surf = test_font.render('My First game',False,(64,64,64))# it take (text informtion,anti-aliase,color)
+#score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surface = pygame.image.load('graphiks/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (600,300))
@@ -45,16 +52,18 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                snail_rect.left = 800                             
+                snail_rect.left = 800  
+                start_time = int(pygame.time.get_ticks() / 1000)                          
 
         
     if game_active:
         #draw all our elements
         screen.blit(sky_surface,(0,0)) #it taik (name,postion)
         screen.blit(ground_surface,(0,300))
-        pygame.draw.rect(screen,'#c0e8ec',score_rect)
-        pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
-        screen.blit(score_surf,score_rect)
+        #pygame.draw.rect(screen,'#c0e8ec',score_rect)
+        #pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+        #screen.blit(score_surf,score_rect)
+        display_score()
 
         snail_rect.x -= 5
         if snail_rect.right <= 0: snail_rect.left = 800
